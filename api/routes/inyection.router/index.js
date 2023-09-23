5b43236d076952ee2e062d2da44f64bb5ef72c8c
Marcelo García <garcia.marcelor@gmail.com>
2023-09-23 17:48:23 -0300

    First commit

---
+++ api/routes/inyection.router/index.js
@@ -0,0 +1,36 @@
+var pool = require('../../mysql'); 
+const register = (router) => {
+    router.post('/inyection', (req, res) => { 
+        // console.log(req.body);
+        try{
+            const id=req.body.id;
+            const tipo=req.body.tipo;
+            const value=req.body.value;
+            const value2=req.body.value2;
+            const value3=req.body.value3;
+            const value4=req.body.value4;
+            const value5=req.body.value5;
+            const value6=req.body.value6;
+            //res.send("El dispositivo id ="+id+" del tipo= "+tipo+" entregó el valor = "+valor);
+            // console.log("El dispositivo id ="+id+" del tipo= "+tipo+" entregó el valor = "+value);
+                pool.query('INSERT INTO TEST_INY (id_dis, tipo, value, value2, value3, value4, value5, value6) values (?,?,?,?,?,?,?,?)', [id, tipo, value,value2,value3,value4,value5,value6], function(err, result, fields) {
+                if (err) {
+                    res.send(err).status(400);
+                    console.log(err);
+                    return;
+                }
+                res.send(result); //Envío solo el primer elemento que vuelve de la BD
+                
+            });
+        }
+        catch{
+            console.log("Parece que hubo bardo")
+        }
+    });
+  
+    return router;
+  };
+  
+  module.exports = {
+    register,
+  };
