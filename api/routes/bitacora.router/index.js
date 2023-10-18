1b4412af6f9a0a6287012e6752be5764bbeed828
Marcelo García <garcia.marcelor@gmail.com>
2023-10-18 06:18:47 -0300

    bitacora endpoint

--- api/routes/bitacora.router/index.js
+++ api/routes/bitacora.router/index.js
@@ -2,34 +2,41 @@ var pool = require('../../mysql');
 const register = (router) => {
     router.post('/post', (req, res) => { 
         console.log("/bitacora/post/" + JSON.stringify(req.body));
-        // try{
-        //     const id=req.body.id;
-        //     const tipo=req.body.tipo;
-        //     const value=req.body.value;
-        //     const value2=req.body.value2;
-        //     const value3=req.body.value3;
-        //     const value4=req.body.value4;
-        //     const value5=req.body.value5;
-        //     const value6=req.body.value6;
-        //     //res.send("El dispositivo id ="+id+" del tipo= "+tipo+" entregó el valor = "+valor);
-        //     // console.log("El dispositivo id ="+id+" del tipo= "+tipo+" entregó el valor = "+value);
-        //         pool.query('INSERT INTO TEST_INY (id_dis, tipo, value, value2, value3, value4, value5, value6) values (?,?,?,?,?,?,?,?)', [id, tipo, value,value2,value3,value4,value5,value6], function(err, result, fields) {
-        //         if (err) {
-        //             res.send(err).status(400);
-        //             console.log(err);
-        //             return;
-        //         }
-        //         res.send(result); //Envío solo el primer elemento que vuelve de la BD
+        try{
+            const titulo=req.body.titulo;
+            const contenido=req.body.contenido;
+            const usuario=req.body.usuario;
+            const idDis=req.body.idDis;
+                pool.query('INSERT INTO bitacora(titulo, contenido, usuarioId, dispositivoId) values (?,?,?,?)', [titulo, contenido, usuario,idDis], function(err, result, fields) {
+                if (err) {
+                    res.send(err).status(400);
+                    console.log(err);
+                    return;
+                }
+                res.send(result); //Envío solo el primer elemento que vuelve de la BD
                 
-        //     });
-        // }
-        // catch{
-        //     console.log("Parece que hubo bardo")
-        // }
+            });
+        }
+        catch{
+            console.log("Parece que hubo bardo")
+        }
+    });
+    router.get('/get/:id/', (req, res) => {
+      pool.query('SELECT * FROM bitacora WHERE dispositivoId=? ORDER BY `fecha` DESC ', [req.params.id], function(err, result, fields) {
+          if (err) {
+              res.send(err).status(400);
+              return;
+          }
+          console.log(result);
+          res.send(result);
+      });
+  
     });
   
     return router;
   };
+
+  
   
   module.exports = {
     register,
